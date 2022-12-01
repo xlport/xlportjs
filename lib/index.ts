e87b325c39d214ddee93e9af160a53a6412fada3
Peter Albert <peter.albert@data2impact.com>
2022-12-01 14:16:02 +0100

    Separate types, add URL to options

--- lib/index.ts
+++ lib/index.ts
@@ -1,82 +1,19 @@
 import { put as putPromise } from 'request-promise'
-
 import { put as putRequest } from 'request'
-import { readFile, ReadStream } from 'fs'
+import { readFile } from 'fs'
 import { extname } from 'path'
 import { promisify } from 'util'
 import { Stream } from 'stream'
-
-export type XlPortClient = {
-  importFromFile: (file: string | Buffer) => Promise<ImportResponse>
-  exportToFile: (data: ExportBody) => Stream
-}
-
-export type ExcelFileExtension = 'xls' | 'xlsx' | 'xlsm' | 'xlsb'
-export type ExcelDefaultMimeType = 'application/vnd.ms-excel'
-export type ExcelMimeType =
-  | ExcelDefaultMimeType
-  | 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-  | 'application/vnd.ms-excel.sheet.macroEnabled.12'
-  | 'application/vnd.ms-excel.sheet.binary.macroEnabled.12'
-
-const excelDefaultMimeType = 'application/vnd.ms-excel'
-type Dictionary<K extends string, T> = { [P in K]?: T }
-export type MimeTypes = Dictionary<ExcelFileExtension, ExcelMimeType> //Record<ExcelFileExtension extends string, ExcelMimeType>;
-
-export type ImportRequest = {
-  properties: string[]
-  tables?: Record<string, string[]>[] | ['*']
-}
-
-export type ExportRawType = Boolean | string | number
-export type ExportDetails = ExportRawType | Record<string, ExportRawType>[]
-export type ExportData = { [key in string]?: ExportDetails }
-
-interface TemplateIdBody {
-  templateId: string
-  data: ExportData
-}
-
-interface UrlBody {
-  templateUrl: string
-  data: ExportData
-}
-
-interface FileBody {
-  template: Buffer | ReadStream
-  data: ExportData
-}
-
-export type ExportBody = FileBody | UrlBody | TemplateIdBody
-
-export type ImportResponse = ImportSuccess | ImportError
-
-export interface ImportError {
-  status: 'error'
-  message: string
-}
-
-export interface ImportSuccess {
-  status: 'success'
-  data: {
-    properties?: Record<string, ImportValue>
-    tables?: Record<string, ImportValue>[]
-  }
-}
-
-export type ImportValue = boolean | string | number
-
-export const defaultImportRequest: ImportRequest = {
-  properties: ['*'],
-  tables: ['*'],
-}
-
-export const mimeTypes: MimeTypes = {
-  xlsx: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-  xlsm: 'application/vnd.ms-excel.sheet.macroEnabled.12',
-  xlsb: 'application/vnd.ms-excel.sheet.binary.macroEnabled.12',
-  xls: 'application/vnd.ms-excel',
-}
+import type {
+  XlPortClientOptions,
+  XlPortClient,
+  ExcelMimeType,
+  ExcelFileExtension,
+  ExportBody,
+  ImportRequest,
+  ImportResponse,
+} from './types'
+import { mimeTypes, excelDefaultMimeType, defaultImportRequest } from './types'
 
 const getExcelMimeType = (path: string): ExcelMimeType =>
   mimeTypes[<ExcelFileExtension>extname(path)] || excelDefaultMimeType
@@ -88,68 +25,71 @@ const loadFile = async (path: string) => ({
     contentType: getExcelMimeType(path),
   },
 })
+export const xlPort = ({ apiKey, url = 'https://xlport.compute.molnify.com' }: XlPortClientOptions): XlPortClient => {
+  url = url.endsWith('/') ? url.slice(0, -1) : url
 
-export const xlPort = (apiKey: string): XlPortClient => ({
-  importFromFile: async (
-    file: string | Buffer,
-    request: ImportRequest = defaultImportRequest,
-  ): Promise<ImportResponse> =>
-    putPromise({
-      url: 'https://xlport.compute.molnify.com/import',
-      headers: {
-        Authorization: 'xlport apikey ' + apiKey,
-      },
-      method: 'put',
-      formData: {
-        file:
-          typeof file === 'string'
-            ? await loadFile(file)
-            : {
-                value: file,
-                options: { filename: 'file.xlsx', contentType: excelDefaultMimeType },
-              },
-        request: {
-          value: Buffer.from(JSON.stringify(request)),
-          options: {
-            filename: 'request.json',
-            contentType: 'application/json',
+  return {
+    importFromFile: async (
+      file: string | Buffer,
+      request: ImportRequest = defaultImportRequest,
+    ): Promise<ImportResponse> =>
+      putPromise({
+        url: `${url}/import`,
+        headers: {
+          Authorization: 'xlport apikey ' + apiKey,
+        },
+        method: 'put',
+        formData: {
+          file:
+            typeof file === 'string'
+              ? await loadFile(file)
+              : {
+                  value: file,
+                  options: { filename: 'file.xlsx', contentType: excelDefaultMimeType },
+                },
+          request: {
+            value: Buffer.from(JSON.stringify(request)),
+            options: {
+              filename: 'request.json',
+              contentType: 'application/json',
+            },
           },
         },
-      },
-    })
-      .then(JSON.parse)
-      .then((response) => {
-        if (!response) throw Error('Response body is empty')
-        return response
-      }),
-  exportToFile: (body: ExportBody): Stream =>
-    putRequest({
-      url: 'https://xlport.compute.molnify.com/export',
-      headers: {
-        Authorization: 'xlport apikey ' + apiKey,
-      },
-      body: 'templateId' in body ? JSON.stringify(body) : 'templateUrl' in body ? JSON.stringify(body) : null,
-      formData:
-        'template' in body
-          ? {
-              template: {
+      })
+        .then(JSON.parse)
+        .then((response) => {
+          if (!response) throw Error('Response body is empty')
+          return response
+        }),
+    exportToFile: (body: ExportBody): Stream =>
+      putRequest({
+        url: `${url}/export`,
+        headers: {
+          Authorization: 'xlport apikey ' + apiKey,
+        },
+        body: 'templateId' in body ? JSON.stringify(body) : 'templateUrl' in body ? JSON.stringify(body) : null,
+        formData:
+          'template' in body
+            ? {
                 template: {
-                  value: body.template,
-                  options: {
-                    filename: 'template.xlsx',
-                    contentType: 'application/vnd.ms-excel',
+                  template: {
+                    value: body.template,
+                    options: {
+                      filename: 'template.xlsx',
+                      contentType: 'application/vnd.ms-excel',
+                    },
                   },
                 },
-              },
-              data: {
-                value: Buffer.from(JSON.stringify(body.data)),
-                options: {
-                  filename: 'data.json',
-                  contentType: 'application/json',
+                data: {
+                  value: Buffer.from(JSON.stringify(body.data)),
+                  options: {
+                    filename: 'data.json',
+                    contentType: 'application/json',
+                  },
                 },
-              },
-            }
-          : undefined,
-      encoding: null,
-    }),
-})
+              }
+            : undefined,
+        encoding: null,
+      }),
+  }
+}
---
+++ lib/types.ts
@@ -0,0 +1,79 @@
+import { Stream } from 'stream'
+import { ReadStream } from 'fs'
+
+export type XlPortClient = {
+  importFromFile: (file: string | Buffer) => Promise<ImportResponse>
+  exportToFile: (data: ExportBody) => Stream
+}
+
+export type ExcelFileExtension = 'xls' | 'xlsx' | 'xlsm' | 'xlsb'
+export type ExcelDefaultMimeType = 'application/vnd.ms-excel'
+export type ExcelMimeType =
+  | ExcelDefaultMimeType
+  | 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+  | 'application/vnd.ms-excel.sheet.macroEnabled.12'
+  | 'application/vnd.ms-excel.sheet.binary.macroEnabled.12'
+
+export const excelDefaultMimeType = 'application/vnd.ms-excel'
+type Dictionary<K extends string, T> = { [P in K]?: T }
+export type MimeTypes = Dictionary<ExcelFileExtension, ExcelMimeType> //Record<ExcelFileExtension extends string, ExcelMimeType>;
+
+export type ImportRequest = {
+  properties: string[]
+  tables?: Record<string, string[]>[] | ['*']
+}
+
+export type ExportRawType = Boolean | string | number
+export type ExportDetails = ExportRawType | Record<string, ExportRawType>[]
+export type ExportData = { [key in string]?: ExportDetails }
+
+interface TemplateIdBody {
+  templateId: string
+  data: ExportData
+}
+
+interface UrlBody {
+  templateUrl: string
+  data: ExportData
+}
+
+interface FileBody {
+  template: Buffer | ReadStream
+  data: ExportData
+}
+
+export type ExportBody = FileBody | UrlBody | TemplateIdBody
+
+export type ImportResponse = ImportSuccess | ImportError
+
+export interface ImportError {
+  status: 'error'
+  message: string
+}
+
+export interface ImportSuccess {
+  status: 'success'
+  data: {
+    properties?: Record<string, ImportValue>
+    tables?: Record<string, ImportValue>[]
+  }
+}
+
+export type ImportValue = boolean | string | number
+
+export const defaultImportRequest: ImportRequest = {
+  properties: ['*'],
+  tables: ['*'],
+}
+
+export const mimeTypes: MimeTypes = {
+  xlsx: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+  xlsm: 'application/vnd.ms-excel.sheet.macroEnabled.12',
+  xlsb: 'application/vnd.ms-excel.sheet.binary.macroEnabled.12',
+  xls: 'application/vnd.ms-excel',
+}
+
+export type XlPortClientOptions = {
+  url: string
+  apiKey: string
+}
