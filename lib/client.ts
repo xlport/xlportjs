afdbc2825b731f7238ce890ea9163a08ff640d30
Peter Albert <peter.albert@data2impact.com>
2023-01-12 11:03:48 +0100

    Replace request with axios

--- lib/client.ts
+++ lib/client.ts
@@ -1,9 +1,8 @@
-import { put as putPromise } from 'request-promise'
-import { put as putRequest } from 'request'
 import { readFile } from 'fs'
 import { extname } from 'path'
 import { promisify } from 'util'
 import { Stream } from 'stream'
+import Axios, { AxiosInstance, toFormData } from 'axios'
 import { Import } from './import.types'
 import { Export } from './export.types'
 
@@ -31,74 +30,75 @@ export type MimeTypes = Record<ExcelFileExtension, ExcelMimeType>
 
 export class Client {
   private config: Required<ClientOptions>
+  private get authHeader() {
+    return 'xlport apikey ' + this.config.apiKey
+  }
+  public axios: AxiosInstance
   constructor(config: ClientOptions) {
     this.config = {
       url: config.url ? config.url.replace(/\/$/, '') : 'https://xlport.compute.molnify.com',
       apiKey: config.apiKey,
     }
+    this.axios = Axios.create({
+      headers: {
+        Authorization: this.authHeader,
+      },
+    })
   }
   public async importFromFile(
     file: string | Buffer,
     request: Import.Request = Import.defaultRequest,
   ): Promise<Import.Response> {
-    return putPromise({
-      url: `${this.config.url}/import`,
-      headers: {
-        Authorization: 'xlport apikey ' + this.config.apiKey,
-      },
-      method: 'put',
-      formData: {
-        file:
-          typeof file === 'string'
-            ? await this.loadFile(file)
-            : {
-                value: file,
-                options: { filename: 'file.xlsx', contentType: excelDefaultMimeType },
-              },
-        request: {
-          value: Buffer.from(JSON.stringify(request)),
-          options: {
-            filename: 'request.json',
-            contentType: 'application/json',
+    return this.axios
+      .put(`${this.config.url}/import`, {
+        formData: {
+          file:
+            typeof file === 'string'
+              ? await this.loadFile(file)
+              : {
+                  value: file,
+                  options: { filename: 'file.xlsx', contentType: excelDefaultMimeType },
+                },
+          request: {
+            value: Buffer.from(JSON.stringify(request)),
+            options: {
+              filename: 'request.json',
+              contentType: 'application/json',
+            },
           },
         },
-      },
-    })
-      .then(JSON.parse)
+      })
+      .then((response) => JSON.parse(response.data))
       .then((response) => {
         if (!response) throw Error('Response body is empty')
         return response
       })
   }
-  public exportToFile(body: Export.Body): Stream {
-    return putRequest({
-      url: `${this.config.url}/export`,
-      headers: {
-        Authorization: 'xlport apikey ' + this.config.apiKey,
-      },
-      body: 'templateId' in body ? JSON.stringify(body) : 'templateUrl' in body ? JSON.stringify(body) : null,
-      formData:
-        'template' in body
-          ? {
+  public exportToFile(body: Export.Body): Promise<Stream> {
+    let data =
+      'templateId' in body || 'templateUrl' in body
+        ? body
+        : toFormData({
+            template: {
               template: {
-                template: {
-                  value: body.template,
-                  options: {
-                    filename: 'template.xlsx',
-                    contentType: 'application/vnd.ms-excel',
-                  },
-                },
-              },
-              data: {
-                value: Buffer.from(JSON.stringify(body.data)),
+                value: body.template,
                 options: {
-                  filename: 'data.json',
-                  contentType: 'application/json',
+                  filename: 'template.xlsx',
+                  contentType: 'application/vnd.ms-excel',
                 },
               },
-            }
-          : undefined,
-      encoding: null,
+            },
+            data: {
+              value: Buffer.from(JSON.stringify(body.data)),
+              options: {
+                filename: 'data.json',
+                contentType: 'application/json',
+              },
+            },
+          })
+    return this.axios.put(`${this.config.url}/export`, data, { responseType: 'stream' }).then((response) => {
+      if (response.status !== 200) throw Error(response.statusText)
+      return response.data as Stream
     })
   }
 
