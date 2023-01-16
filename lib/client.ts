7ee96efc0a0e41c9191d9ebc929f8ec8aec637c7
Peter Albert <peter.albert@data2impact.com>
2023-01-16 13:21:28 +0100

    Add export test

--- lib/client.ts
+++ lib/client.ts
@@ -1,8 +1,13 @@
-import { readFile } from 'fs'
-import { extname } from 'path'
+import { put as putPromise } from 'request-promise'
+import { put as put } from 'request'
 import { promisify } from 'util'
+import { readFile } from 'fs'
+
+// import { createReadStream } from 'fs'
+// import FormData from 'form-data'
+// import Axios, { AxiosInstance, toFormData } from 'axios'
+import { extname } from 'path'
 import { Stream } from 'stream'
-import Axios, { AxiosInstance, toFormData } from 'axios'
 import { Import } from './import.types'
 import { Export } from './export.types'
 
@@ -30,75 +35,137 @@ export type MimeTypes = Record<ExcelFileExtension, ExcelMimeType>
 
 export class Client {
   private config: Required<ClientOptions>
-  private get authHeader() {
-    return 'xlport apikey ' + this.config.apiKey
-  }
-  public axios: AxiosInstance
+  // private get authHeader() {
+  //   return 'xlport apikey ' + this.config.apiKey
+  // }
+  // public axios: AxiosInstance
   constructor(config: ClientOptions) {
+    if (config.apiKey === undefined) throw Error('API Key is undefined')
     this.config = {
       url: config.url ? config.url.replace(/\/$/, '') : 'https://xlport.compute.molnify.com',
       apiKey: config.apiKey,
     }
-    this.axios = Axios.create({
-      headers: {
-        Authorization: this.authHeader,
-      },
-    })
+    // this.axios = Axios.create({
+    //   headers: {
+    //     Authorization: this.authHeader,
+    //   },
+    // })
   }
   public async importFromFile(
     file: string | Buffer,
     request: Import.Request = Import.defaultRequest,
   ): Promise<Import.Response> {
-    return this.axios
-      .put(`${this.config.url}/import`, {
-        formData: {
-          file:
-            typeof file === 'string'
-              ? await this.loadFile(file)
-              : {
-                  value: file,
-                  options: { filename: 'file.xlsx', contentType: excelDefaultMimeType },
-                },
-          request: {
-            value: Buffer.from(JSON.stringify(request)),
-            options: {
-              filename: 'request.json',
-              contentType: 'application/json',
-            },
+    // const formData = new FormData()
+    // if (typeof file === 'string') {
+    //   try {
+    //     const options = { contentType: this.getExcelMimeType(file) }
+    //     formData.append('file.xlsx', createReadStream(file), options)
+    //   } catch (error) {
+    //     console.error(error)
+    //     throw error
+    //   }
+    // } else {
+    //   formData.append('file.xlsx', file, { contentType: excelDefaultMimeType })
+    // }
+    // formData.append('request', JSON.stringify(request))
+
+    // return this.axios
+    //   .put(`${this.config.url}/import`, formData, {
+    //     headers: {
+    //       Accept: 'application/json',
+    //       'Content-Type': 'multipart/form-data',
+    //       ...formData.getHeaders(),
+    //     },
+    //   })
+    //   .then((response) => JSON.parse(response.data))
+    //   .then((response) => {
+    //     if (!response) throw Error('Response body is empty')
+    //     return response
+    //   })
+    return putPromise({
+      url: `${this.config.url}/import`,
+      headers: {
+        Authorization: 'xlport apikey ' + this.config.apiKey,
+      },
+      method: 'put',
+      formData: {
+        file:
+          typeof file === 'string'
+            ? await this.loadFile(file)
+            : {
+                value: file,
+                options: { filename: 'file.xlsx', contentType: excelDefaultMimeType },
+              },
+        request: {
+          value: Buffer.from(JSON.stringify(request)),
+          options: {
+            filename: 'request.json',
+            contentType: 'application/json',
           },
         },
-      })
-      .then((response) => JSON.parse(response.data))
+      },
+    })
+      .then(JSON.parse)
       .then((response) => {
         if (!response) throw Error('Response body is empty')
         return response
       })
   }
-  public exportToFile(body: Export.Body): Promise<Stream> {
-    let data =
-      'templateId' in body || 'templateUrl' in body
-        ? body
-        : toFormData({
-            template: {
+  // public exportToFile(body: Export.Body): Promise<Stream> {
+  // let data =
+  //   'templateId' in body || 'templateUrl' in body
+  //     ? body
+  //     : toFormData({
+  //         template: {
+  //           template: {
+  //             value: body.template,
+  //             options: {
+  //               filename: 'template.xlsx',
+  //               contentType: 'application/vnd.ms-excel',
+  //             },
+  //           },
+  //         },
+  //         data: {
+  //           value: Buffer.from(JSON.stringify(body.data)),
+  //           options: {
+  //             filename: 'data.json',
+  //             contentType: 'application/json',
+  //           },
+  //         },
+  //       })
+  // return this.axios.put(`${this.config.url}/export`, data, { responseType: 'stream' }).then((response) => {
+  //   if (response.status !== 200) throw Error(response.statusText)
+  //   return response.data as Stream
+  // })
+  public exportToFile(body: Export.Body): Stream {
+    return put({
+      url: `${this.config.url}/export`,
+      headers: {
+        Authorization: 'xlport apikey ' + this.config.apiKey,
+      },
+      body: 'templateId' in body ? JSON.stringify(body) : 'templateUrl' in body ? JSON.stringify(body) : null,
+      formData:
+        'template' in body
+          ? {
               template: {
-                value: body.template,
-                options: {
-                  filename: 'template.xlsx',
-                  contentType: 'application/vnd.ms-excel',
+                template: {
+                  value: body.template,
+                  options: {
+                    filename: 'template.xlsx',
+                    contentType: 'application/vnd.ms-excel',
+                  },
                 },
               },
-            },
-            data: {
-              value: Buffer.from(JSON.stringify(body.data)),
-              options: {
-                filename: 'data.json',
-                contentType: 'application/json',
+              data: {
+                value: Buffer.from(JSON.stringify(body.data)),
+                options: {
+                  filename: 'data.json',
+                  contentType: 'application/json',
+                },
               },
-            },
-          })
-    return this.axios.put(`${this.config.url}/export`, data, { responseType: 'stream' }).then((response) => {
-      if (response.status !== 200) throw Error(response.statusText)
-      return response.data as Stream
+            }
+          : undefined,
+      encoding: null,
     })
   }
 
--- tests/client/client.e2e.spec.ts
+++ tests/client/client.e2e.spec.ts
@@ -1,10 +1,64 @@
 import { Client } from '../../lib'
 import * as dotenv from 'dotenv'
-dotenv.config()
+// import { createReadStream, createWriteStream } from 'fs'
+// const streamEqual = require('stream-equal')
+// import { Stream } from 'stream'
+
+import * as simpleExport from './export/simple-export/request.json'
+
+// const writeToFile = (stream: Stream, path: string): Promise<void> => {
+//   const writeStream = stream.pipe(createWriteStream(path))
+//   return new Promise((resolve, reject) => {
+//     writeStream.on('finish', resolve)
+//     writeStream.on('error', reject)
+//   })
+// }
 
 describe('Client', () => {
+  const getClient = () => {
+    dotenv.config({ path: './tests/client/.env' })
+    return new Client({ apiKey: process.env.XLPORT_APIKEY! })
+  }
+
+  it('should throw if API Key is undefined', () => {
+    expect(() => new Client({ apiKey: undefined as any })).toThrow()
+  })
+
   it('should be able to create a client', () => {
-    const client = new Client({ apiKey: process.env.XLPORT_APIKEY! })
+    const client = getClient()
     expect(client).toBeDefined()
   })
+
+  it('should be able to download a file', async () => {
+    const client = getClient()
+    const file = await client.exportToFile(simpleExport)
+    expect(file).toBeDefined()
+  })
+  // it('should be able to download a file and match it to the expected', async () => {
+  //   const file = createReadStream('./tests/client/export/simple-export/expected.xlsx')
+  //   // const xlport = createReadStream('./tests/client/export/simple-export/expected.xlsx')
+  //   const xlport = getClient().exportToFile(simpleExport)
+
+  //   const result = await streamEqual(file, xlport)
+  //   // , (a: any, b: any) => {
+  //   //   console.log(a.data.data)
+  //   //   // const jA = { ...a, data: null }
+  //   //   // const jB = { ...b, data: null }
+  //   //   // console.log(JSON.stringify(a.data).slice(0, 250))
+  //   //   const result = assert.deepStrictEqual(a.data, b.data, 'data is not equal')
+  //   //   console.log('result', result)
+  //   //   return result
+  //   // })
+  //   expect(result).toBeTruthy()
+  // })
+
+  // it.only('save file', async () => {
+  //   const xlport = getClient().exportToFile(simpleExport)
+  //   const target = './tests/client/export/simple-export/test.xlsx'
+  //   await writeToFile(xlport, target)
+  //   // const file1 = createReadStream('./tests/client/export/simple-export/expected.xlsx')
+  //   const file1 = createReadStream(target)
+  //   const file2 = createReadStream('./tests/client/export/simple-export/expected.xlsx')
+  //   expect(await streamEqual(file1, file2)).toBeTruthy()
+  // })
 })
